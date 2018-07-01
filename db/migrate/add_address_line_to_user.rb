430849264fbdb853c6fecc257d9cb88d0a83de1e
kamel Malleh <kamel.mellah@gmail.com>
2018-07-01 20:34:59 +0200

    Add files via upload

---
+++ db/migrate/add_address_line_to_user.rb
@@ -0,0 +1,11 @@
+# -*- encoding : utf-8 -*-
+class AddAddressLineToUser < ActiveRecord::Migration
+  def self.up
+    #address_line uses a text column to support full addresses
+    add_column :users, :address_line, :text, :null => false
+  end
+
+  def self.down
+    remove_column :users, :address_line
+  end
+end
---
+++ db/migrate/add_status_flag_to_user.rb
@@ -0,0 +1,10 @@
+# -*- encoding : utf-8 -*-
+class AddStatusFlagToUser < ActiveRecord::Migration
+  def self.up
+    add_column :users, :status_flag, :string, :null => false
+  end
+
+  def self.down
+    remove_column :users, :status_flag
+  end
+end
