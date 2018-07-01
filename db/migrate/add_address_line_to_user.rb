b5a73e12f0ada686fc932f425094076caf3e3f7d
kamel Malleh <kamel.mellah@gmail.com>
2018-07-01 19:42:01 +0100

    Update add_address_line_to_user.rb

--- db/migrate/add_address_line_to_user.rb
+++ db/migrate/add_address_line_to_user.rb
@@ -2,7 +2,7 @@
 class AddAddressLineToUser < ActiveRecord::Migration
   def self.up
     #address_line uses a text column to support full addresses
-    add_column :users, :address_line, :text, :null => false
+    add_column :users, :address_line, :text, :null => true
   end
 
   def self.down
